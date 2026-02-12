566ea5348115bcead1ba033c635ccda92720255f
gabrieljorda <gabrieleminhamae@hotmail.com>
2026-02-12 20:25:10 -0300

    Adiciona gráficos de vendas ao longo do tempo e por estado em cada região

--- insights.dataset.py
+++ insights.dataset.py
@@ -34,3 +34,35 @@ plt.xlabel('Preço Unitário')
 plt.ylabel('Quantidade de Unidades')
 plt.grid(True)
 plt.show()
+
+#grafico 4 quantidade de unidades vendidas ao longo do tempo
+#convertendo coluna DataPedido para o formato de data
+df["DataPedido"] = pd.to_datetime(df["DataPedido"])
+
+
+plt.figure(figsize=(10,5))
+df.groupby("DataPedido")["Unidades"].sum().plot(kind="line", color="green", marker ='o')
+plt.title('Quantidade de Unidades vendidas ao longo do tempo')
+plt.xlabel('Data do pedido')
+plt.ylabel('Quantidade de Unidades vendidas')
+plt.grid(True)
+plt.show()
+
+
+#grafico 5 quantidade de unidades vendidas por estaddo em cada regiao 
+
+pivot = df.pivot_table(index="Estado",
+                       columns="Regiao",
+                       values="Unidades",
+                       aggfunc="sum",
+                       fill_value=0
+                       )
+
+plt.figure(figsize=(10,6))
+pivot.plot(kind = "bar", stacked=True)
+plt.title('Quantidade de Unidades vendidas por estado em cada região')
+plt.xlabel('Estado')
+plt.ylabel('Quantidade de Unidades vendidas')
+plt.legend(title='Regiao', loc='upper left', bbox_to_anchor=(1.05,1))
+plt.xticks(rotation=45)
+plt.show()
